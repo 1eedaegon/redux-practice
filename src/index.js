06929e278d1fbce45b13f7ec2b272a9d1eb074af
1eedaegon <d8726243@gmail.com>
2021-03-21 17:53:22 +0900

    feat: add counter reducer/subscriber using redux
    일단 리덕스 연습 시작 ㅎ

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
+import {createStore} from "redux";
+
 const plus = document.querySelector('#plus')
 const minus = document.querySelector('#minus')
 const number = document.querySelector('span')
 
-let count = 0;
+number.innerText = 0;
+// Counter reducer
+const counterModifier = (count=0, actions) => {
+  console.log(count, actions);
+  if( actions.type === "PLUS" ) {
+    return count + 1;
+  }
+  if( actions.type === "MINUS") { 
+    return count - 1;
+  }
+  return count;
+}
+const counterStore = createStore(counterModifier);
+const counterOnChange = () => {
+  number.innerText = counterStore.getState()
+}
+const handlePlus = () => {
+  counterStore.dispatch({type: "PLUS"})
+}
+const handleMinus = () => {
+  counterStore.dispatch({type: "MINUS"})
+}
+counterStore.subscribe(counterOnChange)
 
-plus.addEventListener('click', ()=>{
-  (()=>{
-    count = count+1;
-    number.innerText=count;
-  })()
-})
-minus.addEventListener('click', ()=>{
-  (()=>{
-    count = count-1;
-    number.innerText=count;
-  })()
-})
+
+plus.addEventListener('click', handlePlus)
+minus.addEventListener('click', handleMinus)
